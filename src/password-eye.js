9a0cae8d61b5496b1f752864869b4e548328eeb7
Alexandre Maurice <hizaaknewton@gmail.com>
2022-10-21 22:03:45 +0200

    Rework (sauf le todo)

--- src/password-eye.js
+++ src/password-eye.js
@@ -1,11 +1,11 @@
 function showPassword(id) {
     var pass = document.getElementById(id);
-    var img = document.getElementById(id+"EYE");
+    var img = document.getElementById("oeil-"+id);
     if (pass.type === "password") {
       pass.type = "text";
-      img.src="/sources/icon/visibility_on.svg";
+      img.src="sources/icons/visibility_off.svg";
     } else {
       pass.type = "password";
-      img.src="/sources/icon/visibility_off.svg";
+      img.src="sources/icons/visibility_on.svg";
     }
   }
