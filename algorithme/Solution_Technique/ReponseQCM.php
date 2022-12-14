62c7d9160fed2559f0bbadabbab83b4ee512c14f
TauntiO <titouan.brierre@gmail.com>
2022-12-14 15:01:00 +0100

    oopsie

--- algorithme/Solution_Technique/ReponseQCM.php
+++ algorithme/Solution_Technique/ReponseQCM.php
@@ -2,8 +2,24 @@
 <?php
 
 class ReponseQCM extends Reponse{
-
-    //Attributs
+        
+        //Attributs
+        private $reponseQCM;
+        
+        //Constructeur
+        public function __construct($id, $idQuestion, $idEtudiant, $objetReponse, $reponseQCM) {
+            parent::__construct($id, $idQuestion, $idEtudiant, $objetReponse);
+            $this->reponseQCM = $reponseQCM;
+        }
+        
+        //Encapsulation
+        public function getReponseQCM() {
+            return $this->reponseQCM;
+        }
+        
+        public function setReponseQCM($reponseQCM) {
+            $this->reponseQCM = $reponseQCM;
+        }
 }
 
 ?>
