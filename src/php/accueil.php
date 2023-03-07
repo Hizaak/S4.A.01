6b1bfd9cc7568a353bd712dc47d44da5487fc9cf
Leho Errezarret <errezarretleho@gmail.com>
2023-03-07 12:27:28 +0100

    MAJ Compteur de "Accueil"
    Un petit pas pour le BDE mais un grand pas pour S3.A.01

--- src/php/accueil.php
+++ src/php/accueil.php
@@ -44,6 +44,8 @@ $heures = 23;
 $minutes = 36;
 $secondes = 6;
 
+
+
 switch($etatForm)               //TODO : A changer en fonction de la variable indiquant l'état du questionnaire
     {
             case 'inexistant':          //TODO : A adapter
@@ -63,14 +65,14 @@ switch($etatForm)               //TODO : A changer en fonction de la variable in
             case 'accederResultats':           //TODO : A adapter
                 echo "<div id='ferme'>
                 <h2>Merci d'avoir répondu au<br>formulaire !</h2>
-                <button href='#'>Accéder aux résultats</button>
+                <button>Accéder aux résultats</button>
             </div>";
                 break;
 
             case 'modificationReponse':   //TODO : A adapter
                 echo "<div id='ferme'>
                 <h2>Merci d'avoir répondu au<br>formulaire !</h2>
-                <button href='#'>Modifier ma réponse</button>
+                <button>Modifier ma réponse</button>
             </div>";
                 break;
 
@@ -79,35 +81,37 @@ switch($etatForm)               //TODO : A changer en fonction de la variable in
                 <h2>Le formulaire est dispo,<br>réponds-y !</h2>
                 <ul>
                     <li>
-                        <h2 class='Temps'>".$jours."</h2>
+                        <h2 class='Temps' id='jours'></h2>
                         <h3 class='labelHeure'>";
                         if($jours <= 1){echo"jour";}
                         else{echo"jours";}
                         echo"</h3>
                     </li>
                     <li>
-                        <h2 class='Temps'>".$heures."</h2>
-                        <h3 class='labelHeure'>";
-                        if($heures <= 1){echo"heure";}
+                        <h2 class='Temps' id='heures'></h2>";
+                        if($heures <= 1)
+                        {
+                            echo"heure";
+                        }
                         else{echo"heures";}
                         echo"</h3>
                     </li>
                     <li>
-                        <h2 class='Temps'>".$minutes."</h2>
+                        <h2 class='Temps' id='minutes'></h2>
                         <h3 class='labelHeure'>";
                         if($minutes <= 1){echo"minute";}
                         else{echo"minutes";}
                         echo"</h3>
                     </li>
                     <li>
-                        <H2 class='Temps'>".$secondes."</H2>
+                        <H2 class='Temps' id='secondes'></H2>
                         <h3 class='labelHeure'>";
                         if($secondes <= 1){echo"seconde";}
                         else{echo"secondes";}
                         echo"</h3>
                     </li>
                 </ul>
-                <button href='#'>Modifier ma réponse</button>
+                <button>Modifier ma réponse</button>
             </div>";
                 break;
                 case 'formulairePasEncoreOuvert':           //TODO : A adapter
@@ -119,6 +123,79 @@ switch($etatForm)               //TODO : A changer en fonction de la variable in
         break;
         }
         ?>
+        <script>
+            //VARIABLES
+            const dateLimite = new Date(2023,0,0,0,0,0)//year, month, day, hour, minute, second
+            //const dateActuelle = new Date();
+            const dateActuelle = new Date(2022,11,30,23,59,45);
+
+            const differenceMs = Math.abs(dateLimite - dateActuelle);
+
+            days = Math.floor(differenceMs / (1000 * 60 * 60 * 24));
+            hours = Math.floor((differenceMs % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            minutes = Math.floor((differenceMs % (1000 * 60 * 60)) / (1000 * 60));
+            seconds = Math.floor((differenceMs % (1000 * 60)) / 1000);
+
+            document.getElementById("jours").innerHTML = (days);
+            document.getElementById("heures").innerHTML = (hours);
+            document.getElementById("minutes").innerHTML = (minutes);
+            document.getElementById("secondes").innerHTML = (seconds);
+
+
+
+            const countdown = setInterval(function(){
+                // Décrémenter le temps restant
+
+                seconds--;
+
+                if (seconds === -1) // Vérifier si le timer est terminé -- Si non, actualise les minutes, heures et jours.
+                {
+                    seconds = 59;
+                    minutes--;
+                    if (minutes === -1)
+                    {
+                        minutes = 59;
+                        hours--;
+                        if (hours === -1)
+                        {
+                            hours = 23;
+                            days--;
+                            if (days === -1 || hours === 23 || minutes === 59 || seconds === 59)
+                            {
+                                clearInterval(countdown); //Arrêter le timer
+                                seconds = 0;
+                                minutes = 0;
+                                hours = 0;
+                                days = 0;
+
+                                location.reload()
+                            }
+                            else
+                            {
+                                document.getElementById("jours").innerHTML = (days);
+                            }
+                        }
+                        document.getElementById("heures").innerHTML = (hours);
+
+                    }
+                    document.getElementById("minutes").innerHTML = (minutes);
+
+                }
+
+                document.getElementById("secondes").innerHTML = (seconds);
+
+            }, 1000); // Exécuter la fonction toutes les secondes (1000 millisecondes).
+
+
+
+
+
+
+
+
+
+
+        </script>
     </main>
 </body>
 
