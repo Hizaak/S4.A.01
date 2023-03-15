d64efeda85bf253a20f07faf72cb803f8c0c295b
Nicolas <ndargazan001@iutbayonne.univ-pau.fr>
2023-03-16 00:26:46 +0100

    debut form dans accueil

--- src/php/accueil.php
+++ src/php/accueil.php
@@ -217,6 +217,11 @@ switch($etatForm)
                 </ul>
                 <button onclick='window.location.href = \"afficherFormulaire.php\"'>Continuer le formulaire</button>
             </div>";
+            break;
+        case "formulairePasCommence":
+            echo "<div class='info' id='ferme'>
+                <h2>Le formulaire n'est pas encore disponible !</h2>
+            </div>";
 
         }
         ?>
